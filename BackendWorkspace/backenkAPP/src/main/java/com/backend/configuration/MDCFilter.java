f7dd5a22a8270688a9406b9a785b6d07e246cedb
Deepak Arya <deepakarya0155@gmail.com>
2022-04-15 21:34:44 +0530

    Print User name in MDC log from JWT token

--- BackendWorkspace/backenkAPP/src/main/java/com/backend/configuration/MDCFilter.java
+++ BackendWorkspace/backenkAPP/src/main/java/com/backend/configuration/MDCFilter.java
@@ -7,17 +7,27 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.jboss.logging.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.AbstractOAuth2TokenAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MDCFilter implements Filter{
+	
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		MDC.put("appName", "AAAA");
+		HttpServletRequest req=(HttpServletRequest)request;
+		JwtAuthenticationToken token=(JwtAuthenticationToken) req.getUserPrincipal();
+		Jwt jwt=(Jwt) token.getPrincipal();
+		MDC.put("appName", jwt.getClaim("preferred_username").toString());
 		chain.doFilter(request, response);
 	}
 
